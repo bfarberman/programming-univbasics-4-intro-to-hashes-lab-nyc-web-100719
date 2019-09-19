023a4e3cf484f5c9ba556fa894456db73a24b8b1
Brad Farberman <bfarberman@gmail.com>
2019-09-19 02:22:20 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,10 +28,10 @@ end
 
 
 def update_counting_hash(hash, key)
-  if hash["key"] 
-  hash["key"] += 1
-else 
-  hash["key"] = 1
+  hash[key] ? 
+  hash[key] += 1
+  : hash[key] = 1
+  hash 
 
   
   # given a hash an a key as parameters, return an updated hash
